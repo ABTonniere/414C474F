765bbbd2a183aed05a77f1a800591e9e92539a94
Dark-Gouloum <49821955+Dark-Gouloum@users.noreply.github.com>
2024-04-06 23:29:14 +0200

    d

--- Qwirkle/comptePoint.py
+++ Qwirkle/comptePoint.py
@@ -5,34 +5,60 @@ def calc_score_cell_direction(tuiles, dep, direction) :
     nbTuile = 0
     while (i,j) in tuiles :
         nbTuile += 1
-        i -= 1
+        i += direction[0]
+        j += direction[1]
+    print(nbTuile,end=' ')
     return nbTuile
 
 def calc_score_cell(tuiles, ajout, ignore=None) :
     l = ajout[0]
     c = ajout[1]
-    score = 1
+    score = 0
+    inL = False
+    inC = False
+
     nbTuile = calc_score_cell_direction(tuiles, (l,c), (-1,0))
     if nbTuile == 6 :
         score+= 6
-    if ((l - 1), c) != ignore :
+    if (ignore is None) or ((ignore[0] < l) and (ignore[1] == c)) :
         score+= nbTuile
-    nbTuile+= calc_score_cell_direction(tuiles, (l,c), (1,0))
+    elif not (ignore  is  None) :
+        inL = True
+    if nbTuile != 0 :
+        inL = True
+
+    nbTuile = calc_score_cell_direction(tuiles, (l,c), (1,0))
     if nbTuile == 6 :
         score+= 6
-    if ((l + 1), c) != ignore :
+    if (ignore is None) or ((ignore[0] > l) and (ignore[1] == c)) :
         score+= nbTuile
-    nbTuile+= calc_score_cell_direction(tuiles, (l,c), (0,-1))
+    elif not (ignore  is  None) :
+        inL = True
+    if nbTuile != 0 :
+        inL = True
+
+    nbTuile = calc_score_cell_direction(tuiles, (l,c), (0,-1))
     if nbTuile == 6 :
         score+= 6
-    if (l, (c - 1)) != ignore :
+    if (ignore is None) or ((ignore[0] == l) and (ignore[1] < c)) :
         score+= nbTuile
-    nbTuile+= calc_score_cell_direction(tuiles, (l,c), (0,+1))
+    elif not (ignore  is  None) :
+        inC = True
+    if nbTuile != 0 :
+        inC = True
+
+    nbTuile = calc_score_cell_direction(tuiles, (l,c), (0,+1))
     if nbTuile == 6 :
         score+= 6
-    if (l, (c + 1)) != ignore :
+    if (ignore is None) or ((ignore[0] == l) and (ignore[1] > c)) :
         score+= nbTuile
-    return score
+    elif not (ignore  is  None) :
+        inC = True
+    if nbTuile != 0 :
+        inC = True
+
+    print(score)
+    return ((score + 1), inL,inC)
 
 def joue(tuiles, nbL, nbC) :
     ajout = list()
@@ -46,11 +72,25 @@ def joue(tuiles, nbL, nbC) :
                     ajout.append((i,j,ligne[j]))
     if len(ajout) == 0 :
         return 0
-    score = calc_score_cell(tuiles,ajout[0]);
+    oldS,INL,INC = calc_score_cell(tuiles,ajout[0]);
+    score = oldS
     if len(ajout) == 1 :
-        return score
-    for i in range(1, len(ajout)) :
-        score+= calc_score_cell(tuiles,ajout[i],(ajout[i-1][0],ajout[i-1][1]));
+        if INL and INC :
+            score+= 1
+    else :
+        for i in range(1, len(ajout)) :
+            s,inL,inC = calc_score_cell(tuiles,ajout[i],(ajout[i-1][0],ajout[i-1][1]));
+            print(inC,INL,oldS,inL,INC,s)
+            if inC and INL and (oldS != 1) :
+                oldS = s
+                s+= 1
+            elif inL and INC and (oldS != 1) :
+                oldS = s
+                s+= 1
+            else :
+                oldS = s
+            print(score)
+            score+= s
     for i,j,v in ajout :
         tuiles[(i,j)] = v
     return score
