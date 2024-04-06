d5aafddb3b4857492e676a6c32cfa8bb3c0efafe
Dark-Gouloum <49821955+Dark-Gouloum@users.noreply.github.com>
2024-04-06 18:08:55 +0200

    yapt

--- YAPT/yapt.py
+++ YAPT/yapt.py
@@ -6,23 +6,18 @@ def trouver_palindromes(chaine):
     palindromes = []
     i = 0
     taille = len(chaine)
-    while i < (taille - 1) :
-        j = i + 2
-        while j < taille :
+    last = -1
+    for i in range(taille) :
+        pal = -1
+        i2 = i
+        if (i2 < last) :
+            i2 = last
+        for j in range((i2 + 1), taille) :
             if est_palindrome(chaine[i:j]) :
-                i2 = i
-                j2 = j
-                while (0 < i2) and (j2 < taille) and est_palindrome(chaine[i2:j2]) :
-                    i2 -= 1
-                    j2 += 1
-                if i2 < i :
-                    i2 += 1
-                    j2 -= 1
-                palindromes.append((i2, chaine[i2:j2]))
-                i = j2
-                break
-            j+= 1
-        i+= 1
+                pal = j
+        if (pal > (i + 2)) :
+            last = pal
+            palindromes.append((i, chaine[i:pal]))
     return palindromes
 
 
@@ -31,17 +26,21 @@ def trouver_plus_long_palindromes_touchants(chaine):
     if (palindromes is None) or (len(palindromes) == 0) :
         return "NADA"
     palindromes_plus_long = list()
-    for i in range(len(palindromes)) :
-        for j in range((i + 1), len(palindromes)) :
-            pal1 = palindromes[i][1]
-            pal2 = palindromes[j][1]
-            pal =  pal1 + ' ' + pal2
-            tMax = len(pal1)
-            tMin = len(pal2)
-            if tMax < tMin :
-                tMin = tMax
-                tMax = len(pal2)
-            palindromes_plus_long.append((pal,tMax,tMin,palindromes[i][0]))
+    for i in range(len(palindromes) - 2) :
+        deb = palindromes[i][0]
+        pal1 = palindromes[i][1]
+        tMax = len(pal1)
+        j = i + 1
+        while (j < len(palindromes)) and ((deb + tMax) >= palindromes[j][0]) :
+            if (deb + tMax) == palindromes[j][0] :
+                pal2 = palindromes[j][1]
+                pal =  pal1 + ' ' + pal2
+                tMin = len(pal2)
+                if tMax < tMin :
+                    tMin = tMax
+                    tMax = len(pal2)
+                palindromes_plus_long.append((pal,tMax,tMin,deb))
+            j += 1
     if (palindromes_plus_long is None) or (len(palindromes_plus_long) == 0) :
         return "NADA"
     palindromes_plus_long = sorted(palindromes_plus_long, key=lambda x: (-len(x[0]), -x[1], -x[2], x[3]))
