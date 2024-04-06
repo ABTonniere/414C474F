bd466bb582858ac9d0da9e92e2e0c046498b458f
ABTonniere <91685633+ABTonniere@users.noreply.github.com>
2024-04-06 12:34:08 +0200

    Update main.py

--- detricotageDeChaine/main.py
+++ detricotageDeChaine/main.py
@@ -1,26 +1,38 @@
 N = int(input())
-if N < 0 :
+if N < 0:
     N = 1
-elif N > 1000 :
+elif N > 1000:
     N = 1000
 
-for i in range(N) :
+for i in range(N):
+
     entrer = input().split()
-    if len(entrer) != 2 :
+
+    if len(entrer) != 2:
         continue
-    boite,melange = entrer
+
+    boite, melange = entrer
+
     B = len(boite)
-    if (B < 1) or (B > 50) :
+
+    if (B < 1) or (B > 50):
         continue
+
     X = len(melange)
-    if (X < 1) or (X > 10000) :
+
+    if (X < 1) or (X > 10000):
         continue
-    while True :
+
+    while True:
+
         newMelange = melange.replace(boite, "")
-        if newMelange == melange :
+
+        if newMelange == melange:
             break
+
         melange = newMelange
-    if len(melange) == 0 :
+
+    if len(melange) == 0:
         print("OK")
-    else :
+    else:
         print("OUPS")
