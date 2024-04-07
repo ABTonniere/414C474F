702194a5badcb05c94c16352e49a693dd86f73da
Dark-Gouloum <49821955+Dark-Gouloum@users.noreply.github.com>
2024-04-07 06:56:46 +0200

    d

--- wagon/wagon.py
+++ wagon/wagon.py
@@ -1,19 +1,15 @@
-def aChemin(g,u,v) :
+def aChemin(g,cible) :
     file = list()
-    file.append((u,u))
+    file.append((cible,cible))
     visited = set()
     while len(file) != 0 :
-        parent,actu = file.pop(0)
-        visited.add(actu)
-        # Si un voisin du noeud actuel est connecter au noeud d'origine, c'est que ce noeud y est connecter.
-        # Si ce neoud est connecter à l'origine par un chemin déjà pris, cela ne compte pas.
-        for i in g[actu] :
-            if (i == parent) or (i == actu) :
-                continue
-            if (i == v) :
+        parent,n = file.pop(0);
+        visited.add(n)
+        for u in g[n] :
+            if u not in visited :
+                file.append((n,u))
+            if (u == cible) and (u != parent) :
                 return True
-            if (i not in visited) :
-                file.append((actu,i))
     return False
 
 def estCycle(N,M) :
@@ -24,24 +20,46 @@ def estCycle(N,M) :
         # Obtention d'un couple symbolisant un lien
         u = int(liens[i])
         v = int(liens[i+1])
+        if u == v :
+            return True
+        # Vérifier les paramètres
+        if (u < 0) or (v < 0) :
+            return True
+        elif (u >= N) or (u >= N) :
+            return True
         # Si l'un des noeuds ne faisait pas partit du graphe, il est ajouter
         if u not in g :
             g[u] = set()
         if v not in g :
             g[v] = set()
+        # Vérifier si le lien n'existe pas déjà
+        if (v in g[u]) or (u in g[v]) :
+            return True
         # On lie les deux noeuds
         g[u].add(v)
         g[v].add(u)
     # Pour chaques noeuds du graphe, on vérifie si il existe un cycle (chemin passant par 2 autres noeuds et revenant au départ)
     for u in g.keys() :
-        if aChemin(g,u,u) :
+        if aChemin(g,u) :
             return True
     # Si aucun cycle n'à était trouvé, c'est que le graphe n'en à pas.
     return False
 
 R = int(input())
+if R < 1 :
+    R = 1
+elif R > 1000 :
+    R = 1000
 for r in range(R) :
     N,M = map(int, input().split())
+    if N < 1 :
+        N = 1
+    elif N > 1000 :
+        N = 1000
+    if M < 1 :
+        M = 1
+    elif M > 1000 :
+        M = 1000
     if estCycle(N,M) :
         print(1)
     else :
